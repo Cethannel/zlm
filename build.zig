340617d6d62088a0f7101fe19a53c73395ee8b0e
Ethan Nel <cethannel@protonmail.com>
2025-03-10 12:16:01 -0700

    Updated for 0.14.0

--- build.zig
+++ build.zig
@@ -1,8 +1,17 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
     _ = b.addModule("zlm", .{
         .root_source_file = b.path("src/zlm.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
     const test_exe = b.addTest(.{
