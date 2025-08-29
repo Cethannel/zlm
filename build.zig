b082d3e7ee94b8ef0c82ce50200184fb0d6da03e
Ethan Nel <cethannel@protonmail.com>
2025-08-28 20:44:38 -0700

    Updated to 0.15.1

--- build.zig
+++ build.zig
@@ -8,17 +8,19 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("zlm", .{
+    const module = b.addModule("zlm", .{
         .root_source_file = b.path("src/zlm.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const test_exe = b.addTest(.{
-        .root_source_file = b.path("src/test.zig"),
+        .root_module = module,
     });
 
     const test_run = b.addRunArtifact(test_exe);
 
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&test_run.step);
     b.getInstallStep().dependOn(&test_run.step);
 }
--- src/zlm.zig
+++ src/zlm.zig
@@ -13,4 +13,32 @@ pub fn toDegrees(rad: anytype) @TypeOf(rad) {
 }
 
 // export all vectors by-default to f32
-pub usingnamespace SpecializeOn(f32);
+
+const vecf32 = SpecializeOn(f32);
+
+pub const Vec2 = vecf32.Vec2;
+pub const Vec3 = vecf32.Vec3;
+pub const Vec4 = vecf32.Vec4;
+pub const vec2 = vecf32.vec2;
+pub const vec3 = vecf32.vec3;
+pub const vec4 = vecf32.vec4;
+
+pub const Mat2 = vecf32.Mat2;
+pub const Mat3 = vecf32.Mat3;
+pub const Mat4 = vecf32.Mat4;
+
+test Vec4 {
+    const first: Vec4 = Vec4.zero;
+
+    const second = first.add(first);
+
+    try std.testing.expectEqual(first.scale(2), second);
+}
+
+test Vec3 {
+    const first: Vec3 = Vec3.zero;
+
+    const second = first.add(first);
+
+    try std.testing.expectEqual(first.scale(2), second);
+}
